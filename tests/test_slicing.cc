902d80cf77dc74802c8395bc3b13a06471ef6f52
Liu-rj <11911808@mail.sustech.edu.cn>
2022-08-15 13:01:47 +0000

    fix bugs in slicing test

--- tests/test_slicing.cc
+++ tests/test_slicing.cc
@@ -7,15 +7,15 @@ using namespace gs;
 TEST(Slicing, test1)
 {
     Graph A(false);
-    auto options = torch::TensorOptions().dtype(torch::kInt64).device(torch::kCUDA);
+    auto options = torch::TensorOptions().dtype(torch::kInt64).device(torch::kCPU);
     int64_t indptr[] = {0, 1, 1, 3, 4};
     int64_t indices[] = {4, 0, 1, 2};
     int64_t res_indptr[] = {0, 2, 3};
     int64_t res_indices[] = {0, 1, 2};
-    A.LoadCSC(torch::from_blob(indptr, {5}, options), torch::from_blob(indices, {4}, options));
+    A.LoadCSC(torch::from_blob(indptr, {5}, options).to(torch::kCUDA), torch::from_blob(indices, {4}, options).to(torch::kCUDA));
     int64_t column_ids[] = {2, 3};
-    auto subA = A.ColumnwiseSlicing(torch::from_blob(column_ids, {2}, options));
+    auto subA = A.ColumnwiseSlicing(torch::from_blob(column_ids, {2}, options).to(torch::kCUDA));
     auto csc_ptr = subA->GetCSC();
-    EXPECT_TRUE(csc_ptr->indptr.equal(torch::from_blob(res_indptr, {3}, options)));
-    EXPECT_TRUE(csc_ptr->indices.equal(torch::from_blob(res_indices, {3}, options)));
+    EXPECT_TRUE(csc_ptr->indptr.equal(torch::from_blob(res_indptr, {3}, options).to(torch::kCUDA)));
+    EXPECT_TRUE(csc_ptr->indices.equal(torch::from_blob(res_indices, {3}, options).to(torch::kCUDA)));
 }
