7e09c4f2351402f995804f19cecddadf831526eb
Ping Gong <gpzlx1@mail.ustc.edu.cn>
2022-09-28 17:54:28 +0800

    fix bug in list_sampling

--- src/cuda/utils.h
+++ src/cuda/utils.h
@@ -70,13 +70,14 @@ void cub_inclusiveSum(IdType* arrays, int32_t array_length) {
 }
 
 template <typename KeyType, typename ValueType>
-void cub_sortPairsDescending(cub::DoubleBuffer<KeyType> d_keys,
-                             cub::DoubleBuffer<ValueType> d_values,
+void cub_sortPairsDescending(KeyType* d_keys_in, KeyType* d_keys_out,
+                             ValueType* d_values_in, ValueType* d_values_out,
                              int32_t num_items) {
   void* d_temp_storage = NULL;
   size_t temp_storage_bytes = 0;
   cub::DeviceRadixSort::SortPairsDescending(d_temp_storage, temp_storage_bytes,
-                                            d_keys, d_values, num_items);
+                                            d_keys_in, d_keys_out, d_values_in,
+                                            d_values_out, num_items);
   // Allocate temporary storage
   c10::Allocator* cuda_allocator = c10::cuda::CUDACachingAllocator::get();
   c10::DataPtr _temp_data = cuda_allocator->allocate(temp_storage_bytes);
@@ -84,7 +85,8 @@ void cub_sortPairsDescending(cub::DoubleBuffer<KeyType> d_keys,
 
   // Run sorting operation
   cub::DeviceRadixSort::SortPairsDescending(d_temp_storage, temp_storage_bytes,
-                                            d_keys, d_values, num_items);
+                                            d_keys_in, d_keys_out, d_values_in,
+                                            d_values_out, num_items);
 }
 
 /*!
