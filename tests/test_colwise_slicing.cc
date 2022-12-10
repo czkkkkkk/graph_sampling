1dfdb9f57282e8b0cf467dc33875883cc42dd9d2
Ping Gong <gpzlx1@mail.ustc.edu.cn>
2022-12-10 12:34:29 +0800

    add test file for fusedSlicing

--- tests/test_colwise_slicing.cc
+++ tests/test_colwise_slicing.cc
@@ -18,3 +18,20 @@ TEST(ColwiseSlicing, test1)
     EXPECT_TRUE(csc_ptr->indptr.equal(torch::arange(0, 4, options) * 5));
     EXPECT_TRUE(csc_ptr->indices.equal(torch::arange(0, 15, options)));
 }
+
+TEST(FusedRowColSlicing, test1)
+{
+    Graph A(false);
+    auto options = torch::TensorOptions().dtype(torch::kInt64).device(torch::kCUDA);
+    torch::Tensor indptr = torch::arange(0, 21, options) * 5;
+    torch::Tensor indices = torch::arange(0, 100, options);
+    A.LoadCSC(indptr, indices);
+
+    torch::Tensor seeds = torch::arange(0, 5, options);
+
+    auto subA1 = A.ColumnwiseSlicing(seeds)->RowwiseSlicing(seeds);
+    auto subA2 = A.FusionSlicing(seeds);
+
+    EXPECT_TRUE(subA1->GetCSC()->indptr.equal(subA2->GetCSC()->indptr));
+    EXPECT_TRUE(subA1->GetCSC()->indptr.equal(subA2->GetCSC()->indptr));
+}
