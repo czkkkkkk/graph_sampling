4a9ec823c66fcd760f8d961113389da99d7f45a3
Liu-rj <11911808@mail.sustech.edu.cn>
2023-04-23 09:24:39 +0000

    rename pybind API to Upper Camel Case

--- src/pybind.cc
+++ src/pybind.cc
@@ -11,78 +11,78 @@ using namespace gs;
 TORCH_LIBRARY(gs_classes, m) {
   m.class_<Graph>("Graph")
       .def(torch::init<bool>())
-      .def("_CAPI_set_data", &Graph::SetData)
-      .def("_CAPI_get_data", &Graph::GetData)
-      .def("_CAPI_fusion_slicing", &Graph::FusedBidirSlicing)
-      .def("_CAPI_get_num_rows", &Graph::GetNumRows)
-      .def("_CAPI_get_num_cols", &Graph::GetNumCols)
-      .def("_CAPI_get_num_edges", &Graph::GetNumEdges)
-      .def("_CAPI_slicing", &Graph::Slicing)
-      .def("_CAPI_sampling", &Graph::Sampling)
-      .def("_CAPI_sampling_with_probs", &Graph::SamplingProbs)
-      .def("_CAPI_fused_columnwise_slicing_sampling",
+      .def("_CAPI_SetData", &Graph::SetData)
+      .def("_CAPI_GetData", &Graph::GetData)
+      .def("_CAPI_FusionSlicing", &Graph::FusedBidirSlicing)
+      .def("_CAPI_GetNumRows", &Graph::GetNumRows)
+      .def("_CAPI_GetNumCols", &Graph::GetNumCols)
+      .def("_CAPI_GetNumEdges", &Graph::GetNumEdges)
+      .def("_CAPI_Slicing", &Graph::Slicing)
+      .def("_CAPI_Sampling", &Graph::Sampling)
+      .def("_CAPI_SamplingWithProbs", &Graph::SamplingProbs)
+      .def("_CAPI_FusedColumnwiseSlicingSampling",
            &Graph::ColumnwiseFusedSlicingAndSampling)
-      .def("_CAPI_load_csc", &Graph::LoadCSC)
-      .def("_CAPI_load_coo", &Graph::LoadCOO)
-      .def("_CAPI_load_csc_with_col_ids", &Graph::LoadCSCWithColIds)
-      .def("_CAPI_all_valid_node", &Graph::AllValidNode)
-      .def("_CAPI_get_rows", &Graph::GetRows)
-      .def("_CAPI_get_cols", &Graph::GetCols)
-      .def("_CAPI_get_valid_rows", &Graph::GetValidRows)
-      .def("_CAPI_get_valid_cols", &Graph::GetValidCols)
-      .def("_CAPI_get_coo_rows", &Graph::GetCOORows)
-      .def("_CAPI_get_coo_cols", &Graph::GetCOOCols)
-      .def("_CAPI_relabel", &Graph::Relabel)
-      .def("_CAPI_sum", &Graph::Sum)
-      .def("_CAPI_normalize", &Graph::Normalize)
-      .def("_CAPI_divide", &Graph::Divide)
-      .def("_CAPI_metadata", &Graph::MetaData)
-      .def("_CAPI_coo_metadata", &Graph::COOMetaData)
-      .def("_CAPI_csc_metadata", &Graph::CSCMetaData)
-      .def("_CAPI_random_walk", &Graph::RandomWalk)
-      .def("_CAPI_sddmm", &Graph::SDDMM)
-      .def("_CAPI_split", &Graph::Split)
-      .def("_CAPI_get_csc", &Graph::GetCSCTensor)
-      .def("_CAPI_get_coo", &Graph::GetCOOTensor)
-      // .def("_CAPI_batch_slicing", &Graph::BatchSlicing)
-      .def("_CAPI_set_coo", &Graph::SetCOOByTensor)
-      .def("_CAPI_batch_slicing", &Graph::BatchColSlicing)
-      .def("_CAPI_decode", &Graph::Decode)
-      .def("_CAPI_set_metadata", &Graph::SetMetaData)
-      .def("_CAPI_batch_fusion_slicing", &Graph::BatchFusedBidirSlicing)
-      .def("_CAPI_e_div_u_sum", &Graph::EDivUSum);
+      .def("_CAPI_LoadCSC", &Graph::LoadCSC)
+      .def("_CAPI_LoadCOO", &Graph::LoadCOO)
+      .def("_CAPI_LoadCSCWithColIds", &Graph::LoadCSCWithColIds)
+      .def("_CAPI_AllValidNode", &Graph::AllValidNode)
+      .def("_CAPI_GetRows", &Graph::GetRows)
+      .def("_CAPI_GetCols", &Graph::GetCols)
+      .def("_CAPI_GetValidRows", &Graph::GetValidRows)
+      .def("_CAPI_GetValidCols", &Graph::GetValidCols)
+      .def("_CAPI_GetCOORows", &Graph::GetCOORows)
+      .def("_CAPI_GetCooCols", &Graph::GetCOOCols)
+      .def("_CAPI_Relabel", &Graph::Relabel)
+      .def("_CAPI_Sum", &Graph::Sum)
+      .def("_CAPI_Normalize", &Graph::Normalize)
+      .def("_CAPI_Divide", &Graph::Divide)
+      .def("_CAPI_Metadata", &Graph::MetaData)
+      .def("_CAPI_COOMetadata", &Graph::COOMetaData)
+      .def("_CAPI_CSCMetadata", &Graph::CSCMetaData)
+      .def("_CAPI_RandomWalk", &Graph::RandomWalk)
+      .def("_CAPI_SDDMM", &Graph::SDDMM)
+      .def("_CAPI_Split", &Graph::Split)
+      .def("_CAPI_GetCSC", &Graph::GetCSCTensor)
+      .def("_CAPI_GetCOO", &Graph::GetCOOTensor)
+      .def("_CAPI_SetCOO", &Graph::SetCOOByTensor)
+      .def("_CAPI_BatchSlicing", &Graph::BatchSlicing)
+      .def("_CAPI_Decode", &Graph::Decode)
+      .def("_CAPI_SetMetadata", &Graph::SetMetaData)
+      .def("_CAPI_BatchFusionSlicing", &Graph::BatchFusedBidirSlicing)
+      .def("_CAPI_EDivUSum", &Graph::EDivUSum);
 
   m.class_<HeteroGraph>("HeteroGraph")
       .def(torch::init<>())
-      .def("load_from_homo", &HeteroGraph::LoadFromHomo)
-      .def("get_homo_graph", &HeteroGraph::GetHomoGraph)
-      .def("metapath_random_walk_fused", &HeteroGraph::MetapathRandomWalkFused);
+      .def("_CAPI_LoadFromHomo", &HeteroGraph::LoadFromHomo)
+      .def("_CAPI_GetHomoGraph", &HeteroGraph::GetHomoGraph)
+      .def("_CAPI_MetapathRandomWalkFused",
+           &HeteroGraph::MetapathRandomWalkFused);
 }
 
 TORCH_LIBRARY(gs_ops, m) {
-  m.def("list_sampling", &ListSampling);
-  m.def("list_sampling_with_probs", &ListSamplingProbs);
-  m.def("batch_list_sampling_with_probs", &BatchListSamplingProbs);
-  m.def("index_search", &IndexSearch);
-  m.def("SplitByOffset", &SplitByOffset);
-  m.def("IndptrSplitBySize", &gs::impl::SplitIndptrBySizeCUDA);
-  m.def("IndptrSplitByOffset", &gs::impl::SplitIndptrByOffsetCUDA);
-  m.def("BatchConcat", &gs::impl::BatchConcatCUDA);
-  m.def("BatchUnique", &gs::impl::BatchUniqueCUDA);
-  m.def("BatchUniqueByKey", &gs::impl::BatchUniqueByKeyCUDA);
-  m.def("BatchUnique2", &gs::impl::BatchUnique2CUDA);
-  m.def("BatchUniqueByKey2", &gs::impl::BatchUniqueByKey2CUDA);
-  m.def("BatchCSRRelabelByKey", &gs::impl::BatchCSRRelabelByKeyCUDA);
-  m.def("BatchCSRRelabel", &gs::impl::BatchCSRRelabelCUDA);
-  m.def("BatchCOORelabelByKey", &gs::impl::BatchCOORelabelByKeyCUDA);
-  m.def("BatchCOORelabel", &gs::impl::BatchCOORelabelCUDA);
-  m.def("BatchSplit", &gs::impl::BatchSplit2CUDA);
-  m.def("BatchCOOSlicing", &gs::impl::BatchCOOSlicingCUDA);
-  m.def("BatchEncode", &gs::impl::BatchEncodeCUDA);
-  m.def("BatchDecode", &gs::impl::BatchDecodeCUDA);
-  m.def("GetBatchOffsets", &gs::impl::GetBatchOffsets);
-  m.def("COORowSlicingGlobalId", &gs::impl::COORowSlicingGlobalIdCUDA);
-  m.def("_CAPI_unique", &gs::impl::TensorUniqueCUDA);
+  m.def("_CAPI_ListSampling", &ListSampling);
+  m.def("_CAPI_ListSamplingWithProbs", &ListSamplingProbs);
+  m.def("_CAPI_BatchListSamplingWithProbs", &BatchListSamplingProbs);
+  m.def("_CAPI_IndexSearch", &IndexSearch);
+  m.def("_CAPI_SplitByOffset", &SplitByOffset);
+  m.def("_CAPI_IndptrSplitBySize", &gs::impl::SplitIndptrBySizeCUDA);
+  m.def("_CAPI_IndptrSplitByOffset", &gs::impl::SplitIndptrByOffsetCUDA);
+  m.def("_CAPI_BatchConcat", &gs::impl::BatchConcatCUDA);
+  m.def("_CAPI_BatchUnique", &gs::impl::BatchUniqueCUDA);
+  m.def("_CAPI_BatchUniqueByKey", &gs::impl::BatchUniqueByKeyCUDA);
+  m.def("_CAPI_BatchUnique2", &gs::impl::BatchUnique2CUDA);
+  m.def("_CAPI_BatchUniqueByKey2", &gs::impl::BatchUniqueByKey2CUDA);
+  m.def("_CAPI_BatchCSRRelabelByKey", &gs::impl::BatchCSRRelabelByKeyCUDA);
+  m.def("_CAPI_BatchCSRRelabel", &gs::impl::BatchCSRRelabelCUDA);
+  m.def("_CAPI_BatchCOORelabelByKey", &gs::impl::BatchCOORelabelByKeyCUDA);
+  m.def("_CAPI_BatchCOORelabel", &gs::impl::BatchCOORelabelCUDA);
+  m.def("_CAPI_BatchSplit", &gs::impl::BatchSplit2CUDA);
+  m.def("_CAPI_BatchCOOSlicing", &gs::impl::BatchCOOSlicingCUDA);
+  m.def("_CAPI_BatchEncode", &gs::impl::BatchEncodeCUDA);
+  m.def("_CAPI_BatchDecode", &gs::impl::BatchDecodeCUDA);
+  m.def("_CAPI_GetBatchOffsets", &gs::impl::GetBatchOffsets);
+  m.def("_CAPI_COORowSlicingGlobalId", &gs::impl::COORowSlicingGlobalIdCUDA);
+  m.def("_CAPI_Unique", &gs::impl::TensorUniqueCUDA);
 }
 
 namespace gs {}
